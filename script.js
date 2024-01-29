eb577c8b46b1366b86c96f74c89b11e93250a15f
Danasalehhammam <157682210+Danasalehhammam@users.noreply.github.com>
2024-01-29 02:53:14 -0500

    hh

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-const name ="Dana";
+
+const name = "Dana SH";
 let i = 0;
+const interval = setInterval(() => {
+  document.body.innerHTML += name[i];
+  i++;
+  if (i === name.length) clearInterval(interval);
+}, 1000);
