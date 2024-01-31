fafef15454244fc14ae0288b5ad175e802a8e160
Danasalehhammam <157682210+Danasalehhammam@users.noreply.github.com>
2024-01-31 02:56:48 -0500

    up

--- java.js
+++ java.js
@@ -1,1 +1,14 @@
-var slideshows = document.querySelectorAll('[data-component="slide"]);
+var slides = document.querySelectorAll('.slide');
+
+let curr = 0
+
+console.log(slides)
+
+slides.forEach((s,index) => {
+    if (index === curr) {
+        s.style.display = "block"
+    }else {
+        s.style.display = "none"
+
+    }
+})
