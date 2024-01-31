48456aaf810d052612243d56596dccc014802732
Danasalehhammam <157682210+Danasalehhammam@users.noreply.github.com>
2024-01-31 03:45:17 -0500

    hh

--- java.js
+++ java.js
@@ -1,14 +1,42 @@
-var slides = document.querySelectorAll('.slide');
+// var slides = document.querySelectorAll('.slide');
 
-let curr = 0
+// let curr = 0
 
-console.log(slides)
+// console.log(slides)
 
-slides.forEach((s,index) => {
-    if (index === curr) {
-        s.style.display = "block"
-    }else {
-        s.style.display = "none"
+// slides.forEach((s,index) => {
+//     if (index === curr) {
+//         s.style.display = "block"
+//     }else {
+//         s.style.display = "none"
 
-    }
-})
+//     }
+// })
+let slideIndex = 1;
+showSlides(slideIndex);
+
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
